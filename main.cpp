5d95d79a2089d99cd74a9700088d9874b952c62d
yellowday60 <yellowday60@naver.com>
2023-01-12 17:50:35 +0900

    10th commit

--- main.cpp
+++ main.cpp
@@ -24,6 +24,35 @@ void usage(){
     return;
 }
 
+void DumpHex(const void* data, size_t size) {
+	char ascii[17];
+	size_t i, j;
+	ascii[16] = '\0';
+	for (i = 0; i < size; ++i) {
+		printf("%02X ", ((unsigned char*)data)[i]);
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+			ascii[i % 16] = ((unsigned char*)data)[i];
+		} else {
+			ascii[i % 16] = '.';
+		}
+		if ((i+1) % 8 == 0 || i+1 == size) {
+			printf(" ");
+			if ((i+1) % 16 == 0) {
+				printf("|  %s \n", ascii);
+			} else if (i+1 == size) {
+				ascii[(i+1) % 16] = '\0';
+				if ((i+1) % 16 <= 8) {
+					printf(" ");
+				}
+				for (j = (i+1) % 16; j < 16; ++j) {
+					printf("   ");
+				}
+				printf("|  %s \n", ascii);
+			}
+		}
+	}
+}
+
 void sig_handler(int signo){
     attack = false;
     return;
@@ -134,8 +163,13 @@ int main(int argc, char* argv[]){
         set_packet(SSID, packet);
     }
 
+    for(beaconFrame& packet : packets){
+        DumpHex(packet, sizeof(packet));
+    }
+
     while(attack){
         for(beaconFrame& packet : packets){
+            
             int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(packet));
             if (res != 0) fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
             sleep(1);
